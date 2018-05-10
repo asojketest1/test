c005a3a0d830aa62bd493c641ad4f282d0c83bbe
asojketest1 <asojuku.oc+01@gmail.com>
2018-05-10 14:15:29 +0900

    コントローラ、テーブル
    今泉

--- Funana/FunanaController.php
+++ Funana/FunanaController.php
@@ -7,6 +7,7 @@ use Cake\ORM\TableRegistry;
 class FunanaController extends AppController{
 
     public function initialize(){
+        $this->funanas = TableRegistry::get('funanas');
         $this->account = TableRegistry::get('account');
         $this->fruit = TableRegistry::get('fruit');
         $this->skin = TableRegistry::get('skin');
@@ -15,81 +16,56 @@ class FunanaController extends AppController{
 
     public function index(){
         $session = $this->request->session();
-        $data = $this->account->find();
-        $this->set('data',$data);
         $this->set('entity',$this->account->newEntity());
-        if($this->request->is('post')){
-            $data = $this->account->find('all')->where(['id'=>$session->read('Member.id')]);
-            if(count($data) > 0){
-                foreach($data as $obj):
-                if(strcmp($obj->pass, $_POST['pass']) == 0){
-                    $session->write('Member.name', $obj->name);
-                    $session->write('Member.id',$obj->id);
-                    return $this->redirect(['action'=>'choice']);
-                }
-                endforeach;
-            }
-        }
-    }
-
-    //ログイン画面
-    /*public function login(){
-        $this->set('entity',$this->Shoplists->newEntity());
-        if($this->request->is('post')){
-            $board = $this->Shoplists->newEntity($this->request->data);
-            $data = $this->Shoplists->find('all',['conditions'=>['or'=>['article like '=>"%{$board['search']}%",'username like '=>"%{$board['search']}%"]],'order'=>['contributiondate'=>'DESC']]);
-        }
-    }
-    */
-
-    //ログイン後画面
-    /*public function choice(){
-        $session = $this->request->session();
-        $display = $session->read('Member.display');
-        $this->set('entity',$this->shops->newEntity());
-        $data = $this->Shoplists->find('all')->contain(['shops'])->where(['testuser_id'=>$session->read('Member.id')]);
+        $data = $this->account->find('all');
         $this->set('data',$data);
-        $this->set('display',$display);
-        }
-    */
+    }
 
     //ユーザー作成画面
-    public function register(){
+    public function regist(){
         $data = $this->account->find('all');
         $this->set('data',$data);
         $this->set('entity',$this->account->newEntity());
+    }
+    
+    public function registConfirmation(){
+        if($this->request->is('post')){
+            $name = $_POST['NAME'];
+            $mail = $_POST['MAiL'];
+            $pass = $_POST['PASS'];
+            $phone = $_POST['PHONE'];
+            $this->set('entity',$this->account->newEntity());
+            $board = $this->account->newEntity($this->request->data);
+            $this->set('data',$board);
+        }
     }
 
     //実際にユーザーを作成してる画面
     public function addRecord(){
         if($this->request->is('post')){
             $session = $this->request->session();
-            $board = $this->Testusers->newEntity($this->request->data);
-            if($this->Testusers->save($board)){
-                $this->redirect(['action'=>'index']);
+            $board = $this->account->newEntity($this->request->data);
+            if($this->account->save($board)){
+                $account = $this->account->find('all')->where(['NAME'=>$_POST['NAME']])
+                $session->write('id',$account->ID);
+                $this->redirect(['action'=>'friendList']);
             }
             $this->set('entity',$board);
         }
     }
-
-    //買う物登録画面
-    /*public function wantProduct(){
-        $this->set('entity',$this->shops->newEntity());
-        $data = $this->Shoplists->find('all')->contain(['shops']);
-        $this->set('data',$data);
+    
+    //友達一覧
+    public function friendList(){
+        if($this->request->is('post')){
+            $data = $this->account->find('all')->where(['id'=>$session->read('id')]);
+            if(count($data) > 0){
+                foreach($data as $obj):
+                if(strcmp($obj->pass, $_POST['pass']) == 0){
+                    $session->write('id',$obj->id);
+                    return $this->redirect(['action'=>'choice']);
+                }
+                endforeach;
+            }
+        }
     }
-    */
-
-    //買う物一覧表示、削除画面
-    /*public function list(){
-        $board = $this->Testusers->newEntity($this->request->data);
-        $data = $this->Shoplists->find('all')->contain(['product'])->where(['shop_id'=>$board['shopid']]);
-        $this->set('data',$data);
-    }
-    */
-
-    //店種登録画面
-    /*public function shopRegist(){
-
-    }*/
 }
